11ab84e81b09b51a5bab7d73a45df518f052625b
RJ Salamanca <salamancaronito@gmail.com>
2019-07-01 13:26:50 -0400

    added metronome css and js files

---
+++ src/Metronome.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Metronome.css';
+
+class Metronome extends Component {
+    state = {
+        playing: false,
+        count: 0,
+        bpm: 100,
+        beatsPerMeasure: 4
+    }
+
+    render() {
+        let bpm = 100;
+        let playing = false;
+
+        return (
+            <div className="metronome">
+                <div className="bpm-slider">
+                    <div>{bpm} BPM</div>
+                    <input type="range" min="60" max="240" value={bpm} />
+                </div>
+                <button>{playing ? 'STOP' : 'START'}</button>
+            </div>
+        );
+    }
+}
+
+export default Metronome;
